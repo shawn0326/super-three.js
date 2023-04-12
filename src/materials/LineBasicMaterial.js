eba84fa98deb77770b5632db6a0f71562136091a
wzw <wzw9803@163.com>
2023-04-12 13:51:11 +0800

    feat: LineBasicMaterial add support for `map`.

--- src/materials/LineBasicMaterial.js
+++ src/materials/LineBasicMaterial.js
@@ -20,6 +20,8 @@ function LineBasicMaterial( parameters ) {
 
 	this.color = new Color( 0xffffff );
 
+	this.map = null;
+
 	this.linewidth = 1;
 	this.linecap = 'round';
 	this.linejoin = 'round';
@@ -40,6 +42,8 @@ LineBasicMaterial.prototype.copy = function ( source ) {
 	Material.prototype.copy.call( this, source );
 
 	this.color.copy( source.color );
+
+	this.map = source.map;
 
 	this.linewidth = source.linewidth;
 	this.linecap = source.linecap;
